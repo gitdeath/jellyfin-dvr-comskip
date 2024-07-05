bab37c66249331013899abf6f7016cdf6016e65d
Chris <59674699+gitdeath@users.noreply.github.com>
2024-07-05 15:02:43 -0500

    Update jellyfin-drv-comskip.sh

--- config/comskip/jellyfin-drv-comskip.sh
+++ config/comskip/jellyfin-drv-comskip.sh
@@ -10,11 +10,13 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
+# User-definable parameters
+# -------------------------
+
 # Set ffmpeg path to Jellyfin ffmpeg
 __ffmpeg="$(which ffmpeg || echo '/usr/lib/jellyfin-ffmpeg/ffmpeg')"
 
-# Set to skip commericals (mark as chapters) or cut commericals
-# __command="/config/comskip"
+# Set to skip commercials (mark as chapters) or cut commercials
 __command="/config/comcut"
 
 # Set video codec for ffmpeg
@@ -35,6 +37,17 @@ __crf="20"
 # Set Preset
 __preset="slow"
 
+# Define base output directory
+__base_output_dir="/media"
+
+# -------------------------
+
+# Ensure ffmpeg exists
+[ -x "$__ffmpeg" ] || { echo "ffmpeg not found"; exit 1; }
+
+# Ensure the command exists
+[ -x "$__command" ] || { echo "Command for commercial processing not found: $__command"; exit 1; }
+
 # Green Color
 GREEN='\033[0;32m'
 
@@ -44,13 +57,12 @@ NC='\033[0m'
 # Set Path
 __path="${1:-}"
 
-
 PWD="$(pwd)"
 
 die () {
-	echo >&2 "$@"
-	cd "${PWD}"
-	exit 1
+    echo >&2 "$@"
+    cd "${PWD}"
+    exit 1
 }
 
 # verify a path was provided
@@ -62,26 +74,38 @@ __dir="$(dirname "${__path}")"
 __file="$(basename "${__path}")"
 __base="$(basename "${__path}" ".ts")"
 
-# Debbuging path variables
-# printf "${GREEN}path:${NC} ${__path}\ndir: ${__dir}\nbase: ${__base}\n"
+# Verify the .nfo file exists
+__nfo_file="${__dir}/${__base}.nfo"
+[ -f "$__nfo_file" ] || die "NFO file ($__nfo_file) does not exist"
+
+# Extract the title from the .nfo file using xmllint
+__title=$(xmllint --xpath 'string(//*[local-name()="title"])' "${__nfo_file}") || die "Failed to extract title from NFO file"
+
+# Define output directory
+__output_dir="${__base_output_dir}/${__title}"
+
+# Ensure output directory exists
+mkdir -p "${__output_dir}"
+
+# Debugging path variables
+# printf "${GREEN}path:${NC} ${__path}\ndir: ${__dir}\nbase: ${__base}\noutput_dir: ${__output_dir}\n"
 
 # Change to the directory containing the recording
 cd "${__dir}"
 
 # Extract closed captions to external SRT file
 printf "[post-process.sh] %bExtracting subtitles...%b\n" "$GREEN" "$NC"
-$__ffmpeg -f lavfi -i movie="${__file}[out+subcc]" -map 0:1 "${__base}.en.srt"
+"$__ffmpeg" -f lavfi -i "movie=${__file}[out+subcc]" -map 0:1 "${__base}.en.srt"
 
-#comcut/comskip - currently using jellyfin ffmpeg in docker
-$__command --ffmpeg=$__ffmpeg --comskip=/usr/local/bin/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/config/comskip/comskip.ini "${__file}"
-
+# comcut/comskip - currently using jellyfin ffmpeg in docker
+"$__command" --ffmpeg="$__ffmpeg" --comskip="/usr/local/bin/comskip" --lockfile="/tmp/comchap.lock" --comskip-ini="/config/comskip/comskip.ini" "${__file}"
 
 # Transcode to mkv, crf parameter can be adjusted to change output quality
-printf "[post-process.sh] %bTranscoding file..%b\n" "$GREEN" "$NC"
-$__ffmpeg -i "${__file}" -acodec "${__audiocodec}" -b:a "${__bitrate}" -vcodec "${__videocodec}" -vf yadif=parity=auto -crf "${__crf}" -preset "{$__preset}" "${__base}.${__container}"
+printf "[post-process.sh] %bTranscoding file...%b\n" "$GREEN" "$NC"
+"$__ffmpeg" -i "${__file}" -acodec "${__audiocodec}" -b:a "${__bitrate}" -vcodec "${__videocodec}" -vf yadif=parity=auto -crf "${__crf}" -preset "${__preset}" "${__output_dir}/${__base}.${__container}"
 
 # Remove the original recording file
-printf "[post-process.sh] %bRemoving originial file...%b\n" "$GREEN" "$NC"
+printf "[post-process.sh] %bRemoving original file...%b\n" "$GREEN" "$NC"
 rm "${__file}"
 
 # Return to the starting directory
